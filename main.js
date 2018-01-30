70fcf409e1b8d2634839d5b7be4f3780a0669770
tuqiushuang <tuqiushuang@meidaojia.com>
2018-01-30 11:25:02 +0800

    some change

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-// import './css/style.css';
-// import './css/style1.less';
-//import '../js/template.js';
-//import qqlogo from  '../asset/js';
-//console.log(qqlogo);
-//$('body').html('<img src="'+qqlogo+'">');
-
-// import './detail.js';
-// import 'vue';
+import './css/style.less';
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,78 @@
 let ExtractTextPlugin = require("extract-text-webpack-plugin");
-let extractLESS = new ExtractTextPlugin('./public/css/[name].css');
+let extractLESS = new ExtractTextPlugin('css/[name].css');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 module.exports = {
-      entry: {
-				'main':'./main.js',
-				// 'news':'./js/news.js',
-				// 'news_content':'./js/newscontent.js'
-			},
-      output: {
-          path: __dirname, // 输出文件的保存路径
-          filename: './public/js/[name].js' // 输出文件的名称
-      },
+  entry: {
+    'main': './main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'js/[name].js',
+    publicPath: 'https://a-tu9371.github.io/dajia' //上线的地址
+  },
   module: {
-      loaders: [
-	      {
-	          test : /\.(less|css)$/,
-	          loader: ExtractTextPlugin.extract('style', 'css!less')
-	      },
-		// {
-		// 	test: /\.css$/,
-		// 	loader: 'style!css!autoprefixer?{browsers:["last 2 version", "> 1%"]}',
-		// },
-		// {
-		// 	test: /\.less$/,
-		//             use: [{
-		//                 loader: "style-loader" // creates style nodes from JS strings
-		//             }, {
-		//                 loader: "css-loader" // translates CSS into CommonJS
-		//             }, {
-		//                 loader: "less-loader" // compiles Less to CSS
-		//             }]
-		// },
-		// {
-		// 	test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
-		// 	loader: 'file?name=./fonts/[name].[ext]',
-		// },
-		{
-		    test: /\.js?$/,
-		    exclude: /(node_modules|bower_components)/,
-		    loaders: [
-		      'babel?presets[]=react,presets[]=es2015,presets[]=stage-1'
-		    ]
-		}
-		,
-		{
-		    test: /\.json$/,
-		    loader: "json"
-		}
+    loaders: [{
+        test: /\.(less|css)$/,
+        loader: ExtractTextPlugin.extract('style', 'css!less')
+      },
+      // {
+      // 	test: /\.css$/,
+      // 	loader: 'style!css!autoprefixer?{browsers:["last 2 version", "> 1%"]}',
+      // },
+      // {
+      // 	test: /\.less$/,
+      //             use: [{
+      //                 loader: "style-loader" // creates style nodes from JS strings
+      //             }, {
+      //                 loader: "css-loader" // translates CSS into CommonJS
+      //             }, {
+      //                 loader: "less-loader" // compiles Less to CSS
+      //             }]
+      // },
+      // {
+      // 	test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
+      // 	loader: 'file?name=./fonts/[name].[ext]',
+      // },
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: [
+          'babel?presets[]=react,presets[]=es2015,presets[]=stage-1'
+        ]
+      }, {
+        test: /\.json$/,
+        loader: "json"
+      }, {
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            publicPath: 'https://a-tu9371.github.io/dajia'
+          }
+        }]
+      }
 
-	]
-	}
-	,
-	plugins: [
-	// new ExtractTextPlugin("./public/css/[name].css")
-	// extractCSS,
-	extractLESS
-	]
-	,
-	// externals: {
-	// 'react': 'React' ,
-	// 'react-dom': 'ReactDOM' ,
-	// 'jquery': 'jQuery' 
-	// }
+    ]
+  },
+  plugins: [
+    // new ExtractTextPlugin("css/[name].less"),
+    // extractCSS,
+    extractLESS,
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      // inject: 'head' // 打包后script标签放在了head标签里，false为不插入打包后文件
+      minify: { //压缩配置
+        removeComments: true, //删除注释
+        collapseWhitespace: true //删除空行
+      }
+    })
+  ],
+  // externals: {
+  // 'react': 'React' ,
+  // 'react-dom': 'ReactDOM' ,
+  // 'jquery': 'jQuery'
+  // }
 }
-  
