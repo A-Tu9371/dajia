c2b10dfeafc7e90d38ced7a4f857e8ecfe490b75
tuqiushuang <tuqiushuang@meidaojia.com>
2018-01-30 11:33:33 +0800

    change

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,65 @@
 let ExtractTextPlugin = require("extract-text-webpack-plugin");
-let extractLESS = new ExtractTextPlugin('css/[name].css');
+let extractLESS = new ExtractTextPlugin('./public/css/[name].css');
 module.exports = {
-  entry: {
-    'main': './main.js',
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: 'js/[name].js',
-    publicPath: 'https://a-tu9371.github.io/dajia' //上线的地址
-  },
-  module: {
-    loaders: [{
-        test: /\.(less|css)$/,
-        loader: ExtractTextPlugin.extract('style', 'css!less')
+      entry: {
+				'main':'./main.js',
+				// 'news':'./js/news.js',
+				// 'news_content':'./js/newscontent.js'
+			},
+      output: {
+          path: __dirname, // 输出文件的保存路径
+          filename: './public/js/[name].js' // 输出文件的名称
       },
-      // {
-      // 	test: /\.css$/,
-      // 	loader: 'style!css!autoprefixer?{browsers:["last 2 version", "> 1%"]}',
-      // },
-      // {
-      // 	test: /\.less$/,
-      //             use: [{
-      //                 loader: "style-loader" // creates style nodes from JS strings
-      //             }, {
-      //                 loader: "css-loader" // translates CSS into CommonJS
-      //             }, {
-      //                 loader: "less-loader" // compiles Less to CSS
-      //             }]
-      // },
-      // {
-      // 	test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
-      // 	loader: 'file?name=./fonts/[name].[ext]',
-      // },
-      {
-        test: /\.js?$/,
-        exclude: /(node_modules|bower_components)/,
-        loaders: [
-          'babel?presets[]=react,presets[]=es2015,presets[]=stage-1'
-        ]
-      }, {
-        test: /\.json$/,
-        loader: "json"
-      }
+  module: {
+      loaders: [
+	      {
+	          test : /\.(less|css)$/,
+	          loader: ExtractTextPlugin.extract('style', 'css!less')
+	      },
+		// {
+		// 	test: /\.css$/,
+		// 	loader: 'style!css!autoprefixer?{browsers:["last 2 version", "> 1%"]}',
+		// },
+		// {
+		// 	test: /\.less$/,
+		//             use: [{
+		//                 loader: "style-loader" // creates style nodes from JS strings
+		//             }, {
+		//                 loader: "css-loader" // translates CSS into CommonJS
+		//             }, {
+		//                 loader: "less-loader" // compiles Less to CSS
+		//             }]
+		// },
+		// {
+		// 	test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
+		// 	loader: 'file?name=./fonts/[name].[ext]',
+		// },
+		{
+		    test: /\.js?$/,
+		    exclude: /(node_modules|bower_components)/,
+		    loaders: [
+		      'babel?presets[]=react,presets[]=es2015,presets[]=stage-1'
+		    ]
+		}
+		,
+		{
+		    test: /\.json$/,
+		    loader: "json"
+		}
 
-    ]
-  },
-  plugins: [
-    // new ExtractTextPlugin("css/[name].less"),
-    // extractCSS,
-    extractLESS
-  ],
-  // externals: {
-  // 'react': 'React' ,
-  // 'react-dom': 'ReactDOM' ,
-  // 'jquery': 'jQuery'
-  // }
+	]
+	}
+	,
+	plugins: [
+	// new ExtractTextPlugin("./public/css/[name].css")
+	// extractCSS,
+	extractLESS
+	]
+	,
+	// externals: {
+	// 'react': 'React' ,
+	// 'react-dom': 'ReactDOM' ,
+	// 'jquery': 'jQuery' 
+	// }
 }
+  
